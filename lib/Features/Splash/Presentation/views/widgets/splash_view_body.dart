b63d47afcc554e88005a8a3432e6c947eb649955
HasanxElwan <xh.elwan.hx@gmail.com>
2023-01-08 01:14:24 +0200

    First Commit

---
+++ lib/Features/Splash/Presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/Core/utils/assests.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [Image.asset(AssetsData.logo)],
+    );
+  }
+}
